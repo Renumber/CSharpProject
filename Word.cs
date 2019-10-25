6b4667bf28ebd45007f8c298cbcef43789b51fe4
renumber <jwook9331@naver.com>
2019-10-25 15:33:26 +0900

    test

--- Word.cs
+++ Word.cs
@@ -1,9 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 class Word{
     static void Main(string[] args){
             Console.WriteLine("Hello World!");
-        }
     }
+}
