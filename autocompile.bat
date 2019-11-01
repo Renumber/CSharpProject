c964d92105c7a7f2885f47c04e920224c14a548a
renumber <jwook9331@naver.com>
2019-11-01 22:37:55 +0900

    test

--- autocompile.bat
+++ autocompile.bat
@@ -1,4 +1,4 @@
 @echo off
-C:\Windows\Microsoft.Net\Framework\v4.0.30319\csc.exe \out:Word.exe \t:exe Word.csc
+C:\Windows\Microsoft.Net\Framework\v4.0.30319\csc.exe /out:Word.exe /t:exe Word.cs
 Word.exe
 PAUSE
