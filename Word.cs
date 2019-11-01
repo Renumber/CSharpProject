1dcc4150cabc5f7009c3f842c6730f49f51c8694
renumber <jwook9331@naver.com>
2019-11-01 23:12:54 +0900

    test

--- Word.cs
+++ Word.cs
@@ -45,7 +45,7 @@ class Word{
 public class MainFrame : Form{
     private const int FORM_HEIGHT = 200;
     private const int FORM_LENGTH = 300;
-    private bool sysFlag, runFlag, WordFlag;
+    private bool sysFlag, runFlag, wordFlag;
     private Label wordLabel, meanLabel;
     private Button preButton, stopButton, nextButton;
 
@@ -60,6 +60,9 @@ public class MainFrame : Form{
         initializeComponent();
     }
     private void initializeComponent(){
+        int margin = 15;
+        int buttonSize = (int)((FORM_LENGTH-margin*4)/3);
+
         wordLabel.Text = "word";
         wordLabel.Size = new Size(FORM_LENGTH-24, 40);
         wordLabel.Location = new Point(10,10);
@@ -74,16 +77,110 @@ public class MainFrame : Form{
         meanLabel.Font = new Font("Serif", 18, FontStyle.Bold);
         meanLabel.TextAlign = ContentAlignment.MiddleCenter;
 
+        preButton.Text = "이전";
+        preButton.Size = new Size((buttonSize), 30);
+        preButton.Location = new Point(margin, 130);
+        preButton.Font = new Font("Serif", 10, FontStyle.Bold);
+        preButton.TextAlign = ContentAlignment.MiddleCenter;
+        preButton.KeyDown += new KeyEventHandler(this.keyDown);
+        preButton.Click += new EventHandler(this.btnClick);
+
+        stopButton.Text = "정지/시작";
+        stopButton.Size = new Size((buttonSize), 30);
+        stopButton.Location = new Point(margin * 2 + buttonSize, 130);
+        stopButton.Font = new Font("Serif", 10, FontStyle.Bold);
+        stopButton.TextAlign = ContentAlignment.MiddleCenter;
+        stopButton.KeyDown += new KeyEventHandler(this.keyDown);
+        stopButton.Click += new EventHandler(this.btnClick);
+
+        nextButton.Text = "다음";
+        nextButton.Size = new Size((buttonSize), 30);
+        nextButton.Location = new Point(margin * 3 + buttonSize * 2, 130);
+        nextButton.Font = new Font("Serif", 10, FontStyle.Bold);
+        nextButton.TextAlign = ContentAlignment.MiddleCenter;
+        nextButton.KeyDown += new KeyEventHandler(this.keyDown);
+        nextButton.Click += new EventHandler(this.btnClick);
+
         this.Controls.Add(wordLabel);
+        this.Controls.Add(meanLabel);
+        this.Controls.Add(preButton);
+        this.Controls.Add(stopButton);
+        this.Controls.Add(nextButton);
 
         this.Size = new Size(FORM_LENGTH, FORM_HEIGHT);
+        this.KeyDown += new KeyEventHandler(this.keyDown);
         this.Opacity = 0.9;
         this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
         this.StartPosition = FormStartPosition.CenterScreen;
         this.TopMost = true;
+
+        this.Closing += new CancelEventHandler(this.formClosing);
+
+        Thread thread = new Thread(wordThread);
+        thread.Start();
+
+    }
+    private void keyDown(object sender, KeyEventArgs e){
+        if(e.KeyCode == Keys.Escape){
+            sysFlag = false;
+            this.Close();
+        }else if(e.KeyCode == Keys.R){
+            Word.wordDic.shuffle();
+            Word.wordDic.showAll();
+        }else if(e.KeyCode == Keys.F1){
+            runFlag = false;
+            wordFlag = false;
+        }
+    }
+    private void btnClick(object sender, EventArgs e){
+        string btnText = ((Button)sender).Text;
+        string word = "";
+        switch(btnText){
+            case "이전":
+                runFlag = false;
+                word = Word.wordDic.getPre();
+                wordLabel.Text = word;
+                meanLabel.Text = Word.wordDic.getMean(word);
+                wordFlag = false;
+                break;
+            case "정지/시작":
+                runFlag = !runFlag;
+                Console.WriteLine(runFlag);
+                break;
+            case "다음":
+                runFlag = false;
+                word = Word.wordDic.getNext();
+                wordLabel.Text = word;
+                meanLabel.Text = Word.wordDic.getMean(word);
+                wordFlag = false;
+                runFlag = true;
+                break;
+            default :
+                break;
+        }
+    }
+    private void formClosing(object sender, CancelEventArgs e){
+        Console.WriteLine("끝!");
+        sysFlag = false;
+    }
+    private void wordThread(){
+        wordFlag = false;
+        string word = "";
+        while(sysFlag == true){
+            if(runFlag == true && wordFlag == false){
+                word = Word.wordDic.getNext();
+                wordLabel.Text = word;
+                meanLabel.Text = " ";
+                wordFlag = true;
+                Thread.Sleep(1700);
+            }if(runFlag == true && wordFlag == true){
+                meanLabel.Text = Word.wordDic.getMean(word);
+                wordFlag = false;
+                Thread.Sleep(2000);
+            }
+        }
     }
 }
-
 [Serializable]
 public class WordDic{
     private Dictionary<string,string> dic;
@@ -112,7 +209,7 @@ public class WordDic{
             keys[n] = temp;
         }
     }
-    //before showall, do shuffle
+    //before showAll, do shuffle
     public void showAll(){
         foreach(string str in keys){
             Console.WriteLine(str + "\t" + dic[str]);
