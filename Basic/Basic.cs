f5eb9abe1ddebcbf5b2428cb30a812d0ceb3bf15
unknown <jwook9331@naver.com>
2019-11-06 16:58:11 +0900

    test

--- Basic/Basic.cs
+++ Basic/Basic.cs
@@ -10,11 +10,11 @@ using System.IO;
 using System.Runtime.Serialization.Json;
 
 class Basic{
-    public static const int TOP_HEIGHT = 50;
-    public static const int FORM_HEIGHT = 700 + TOP_HEIGHT;
-    public static const int FORM_LENGTH = 700;
-    public static const int INTERVAL_NUM = 10;
-    private static const string FILE_NAME = "Basic" + @"database.db";
+    public static int TOP_HEIGHT = 50;
+    public static int FORM_HEIGHT = 700 + TOP_HEIGHT;
+    public static int FORM_LENGTH = 700;
+    public static int INTERVAL_NUM = 10;
+    private static string FILE_NAME = "Basic" + @"database.db";
         
     public static Rank rank;
     public static List<Bullet> bullets;
@@ -43,7 +43,7 @@ class Basic{
         Console.WriteLine("save");
     }
 }
-[serializable]
+[Serializable]
 public class Rank{
     public int length = 9;
     public int[] scores;
@@ -66,113 +66,172 @@ public class Rank{
     }
 }
 
-public class MainForm : Form{
+public class RankForm : Form{
     private const int FORM_HEIGHT = 200;
-    private const int FORM_LENGTH = 300;
-    private bool sysFlag, runFlag, wordFlag;
-    private Label wordLabel, meanLabel;
-    private Button preButton, stopButton, nextButton;
+    private const int FORM_LENGTH = 150;
+    private ListBox rankBox;
 
-    public MainForm(){
-        wordLabel = new Label();
-        meanLabel = new Label();
-        preButton = new Button();
-        nextButton = new Button();
-        stopButton = new Button();
-        sysFlag = true;
-        runFlag = true;
+    public RankForm(){
+        rankBox = new ListBox();
         initializeComponent();
     }
     private void initializeComponent(){
-        int margin = 15;
-        int buttonSize = (int)((FORM_LENGTH-margin*4)/3);
+        rankBox.Size = new Size(FORM_LENGTH, FORM_HEIGHT);
+        rankBox.Location = new Point(0,0);
+        rankBox.Font = new Font("Serif", 10);
+        rankBox.KeyDown += new KeyEventHandler(this.keyDown);
 
-        wordLabel.Text = "word";
-        wordLabel.Size = new Size(FORM_LENGTH-24, 40);
-        wordLabel.Location = new Point(10,10);
-        wordLabel.BackColor = Color.LightGreen;
-        wordLabel.Font = new Font("Serif", 18, FontStyle.Bold);
-        wordLabel.TextAlign = ContentAlignment.MiddleCenter;
-
-        meanLabel.Text = "mean";
-        meanLabel.Size = new Size(FORM_LENGTH-24, 40);
-        meanLabel.Location = new Point(10,60);
-        meanLabel.BackColor = Color.LightBlue;
-        meanLabel.Font = new Font("Serif", 18, FontStyle.Bold);
-        meanLabel.TextAlign = ContentAlignment.MiddleCenter;
-
-        preButton.Text = "이전";
-        preButton.Size = new Size((buttonSize), 30);
-        preButton.Location = new Point(margin, 130);
-        preButton.Font = new Font("Serif", 10, FontStyle.Bold);
-        preButton.TextAlign = ContentAlignment.MiddleCenter;
-        preButton.KeyDown += new KeyEventHandler(this.keyDown);
-        preButton.Click += new EventHandler(this.btnClick);
-
-        stopButton.Text = "정지/시작";
-        stopButton.Size = new Size((buttonSize), 30);
-        stopButton.Location = new Point(margin * 2 + buttonSize, 130);
-        stopButton.Font = new Font("Serif", 10, FontStyle.Bold);
-        stopButton.TextAlign = ContentAlignment.MiddleCenter;
-        stopButton.KeyDown += new KeyEventHandler(this.keyDown);
-        stopButton.Click += new EventHandler(this.btnClick);
-
-        nextButton.Text = "다음";
-        nextButton.Size = new Size((buttonSize), 30);
-        nextButton.Location = new Point(margin * 3 + buttonSize * 2, 130);
-        nextButton.Font = new Font("Serif", 10, FontStyle.Bold);
-        nextButton.TextAlign = ContentAlignment.MiddleCenter;
-        nextButton.KeyDown += new KeyEventHandler(this.keyDown);
-        nextButton.Click += new EventHandler(this.btnClick);
-
-        this.Controls.Add(wordLabel);
-        this.Controls.Add(meanLabel);
-        this.Controls.Add(preButton);
-        this.Controls.Add(stopButton);
-        this.Controls.Add(nextButton);
-
-        this.Size = new Size(FORM_LENGTH, FORM_HEIGHT);
-        this.KeyDown += new KeyEventHandler(this.keyDown);
-        this.Opacity = 0.9;
+        for(int i = 0; i < Basic.rank.length; i++){
+            rankBox.Items.Add((i+1) + "등 : " + Basic.rank.scores[i] + "점");
+        }
+        this.Controls.Add(rankBox);
+        this.Text = "당신의 점수는?";
+        this.Size = new Size(FORM_LENGTH, FORM_HEIGHT); 
         this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
-        this.StartPosition = FormStartPosition.CenterScreen;
-        this.TopMost = true;
-
-        this.Closing += new CancelEventHandler(this.formClosing);
-
-        Thread thread = new Thread(wordThread);
-        thread.Start();
-
     }
     private void keyDown(object sender, KeyEventArgs e){
         if(e.KeyCode == Keys.Escape){
-            sysFlag = false;
             this.Close();
-        }else if(e.KeyCode == Keys.R){
-            Word.wordDic.shuffle();
-            Word.wordDic.showAll();
-        }else if(e.KeyCode == Keys.F1){
-            runFlag = false;
-            wordFlag = false;
-            SetFrame SetFrame = new SetFrame();
-            SetFrame.ShowDialog();
         }
     }
-    private void btnClick(object sender, EventArgs e){
-        string btnText = ((Button)sender).Text;
-        string word = "";
-        switch(btnText){
-        }
-    }
+}
 
+public class MainForm : Form{
+    bool topMouseDownFlag, gameFlag;
+    bool upFlag, downFlag, rightFlag, leftFlag;
+    int mouseX, mouseY, score;
+    Label topLabel;
+    Brush borderBrush;
+    Random rnd;
+
+    public MainForm(){
+        score = 0;
+        topMouseDownFlag = false;
+        gameFlag = false;
+        topLabel = new Label();
+        borderBrush = new SolidBrush(Color.LightGreen);
+        rnd = new Random();
+        int x , y, r, angle, vel;
+        Brush bulletBrush;
+        for(int i = 0; i < 10; i++){
+            x = 30;
+            y = 30;
+            r = rnd.Next(1, 31);
+            angle = rnd.Next(0, 360);
+            vel = rnd.Next(3, 7);
+            bulletBrush = new SolidBrush(Color.FromArgb(rnd.Next(0,256), rnd.Next(0,256), rnd.Next(0,256)));
+            Basic.bullets.Add(new Bullet(x, y, r, angle, vel, bulletBrush));
+        }
+        initializeComponent();
+    }
+    private void initializeComponent(){
+        topLabel.Text = "Hello?";
+        topLabel.Size = new Size(Basic.FORM_LENGTH, Basic.FORM_HEIGHT);
+        topLabel.Location = new Point(0,0);
+        topLabel.BackColor = Color.LightGreen;
+        topLabel.Font = new Font("Serif", 16, FontStyle.Bold);
+        topLabel.TextAlign = ContentAlignment.MiddleCenter;
+//        topLabel.MouseDown += new MouseEventHandler(this.topDown);
+//        topLabel.MouseMove += new MouseEventHandler(this.topMove);
+//        topLabel.MouseUp += new MouseEventHandler(this.topUp);
+
+        this.Controls.Add(topLabel);
+
+        this.Size = new Size(Basic.FORM_LENGTH, Basic.FORM_HEIGHT);
+        this.Opacity = 0.9;
+        this.StartPosition = FormStartPosition.CenterScreen;
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.KeyDown += new KeyEventHandler(this.keyDown);
+        this.KeyUp += new KeyEventHandler(this.keyUp);
+        this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        this.SetStyle(ControlStyles.UserPaint, true);
+        this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+        this.UpdateStyles();
+
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        timer.Interval = Basic.INTERVAL_NUM;
+        timer.Tick += new EventHandler(update);
+        timer.Start();
+
+        gameFlag = true;
+    }
+    protected override void OnPaintBackground(PaintEventArgs e){
+        base.OnPaint(e);
+        e.Graphics.Clear(Color.White);
+        e.Graphics.FillRectangle(borderBrush, 0, 0, 2, Basic.FORM_HEIGHT);
+        e.Graphics.FillRectangle(borderBrush, 0, Basic.FORM_HEIGHT - 2, Basic.FORM_LENGTH, 2);
+        e.Graphics.FillRectangle(borderBrush, Basic.FORM_LENGTH - 2, 0, 2, Basic.FORM_HEIGHT);
+        foreach (Bullet bullet in Basic.bullets){
+            e.Graphics.FillEllipse(bullet.brush, bullet.x, bullet.y, bullet.r * 2, bullet.r * 2);
+        }
+    }
+    private void update(object sender, EventArgs e){
+        if(gameFlag){
+            foreach (Bullet bullet in Basic.bullets){
+                bullet.update();
+            }
+        }
+        Invalidate();
+    }
+    private void gameOver(){
+        gameFlag = false;
+        MessageBox.Show("Game Over! \n" + score + " 점\n" + "R 키로 재시작");
+        Basic.rank.addScore(score);
+        Basic.save();
+        //todo
+        RankForm rankForm = new RankForm();
+        rankForm.ShowDialog();
+    }
+    private void restart(){
+        //todo
+    }
+    private void topDown(object sender, MouseEventArgs e){
+        topMouseDownFlag = true;
+        mouseX = e.X;
+        mouseY = e.Y;
+    }
+    private void topMove(object sender, EventArgs e){
+        if(topMouseDownFlag){
+            this.SetDesktopLocation(Cursor.Position.X - mouseX, Cursor.Position.Y - mouseY);
+        }
+    }
+    private void topUp(object sender, MouseEventArgs e){
+        topMouseDownFlag = false;
+    }
+    private void keyDown(object sender, KeyEventArgs e){
+        if(e.KeyCode == Keys.Up || upFlag){
+            upFlag = true;
+        }if(e.KeyCode == Keys.Down || downFlag){
+            downFlag = true;
+        }if(e.KeyCode == Keys.Right || rightFlag){
+            rightFlag = true;
+        }if(e.KeyCode == Keys.Left || leftFlag){
+            leftFlag = true;
+        }if(e.KeyCode == Keys.R){
+            restart();
+        }if(e.KeyCode == Keys.Escape){
+            gameFlag = false;
+            this.Close();
+        }if(e.KeyCode == Keys.F1){
+            gameOver();
+        }
+    }
+    private void keyUp(object sender, KeyEventArgs e){
+        if(e.KeyCode == Keys.Up){
+            upFlag = false;
+        }if(e.KeyCode == Keys.Down){
+            downFlag = false;
+        }if(e.KeyCode == Keys.Right){
+            rightFlag = false;
+        }if(e.KeyCode == Keys.Left){
+            leftFlag = false;
+        }
+    }
 }
 public class Bullet{
     public float x, y, r, vel;
     public Brush brush;
     public Vector vector;
-    public Bullet(){
-        this(100, 100, 10, 80, 5, new SolidBrush(Color.FromArgb(100, 100, 255)));
-    }
     public Bullet(float x, float y, float r, float angle, float vel, Brush brush){
         this.r = r;
         this.x = x - r;
@@ -181,13 +240,13 @@ public class Bullet{
         this.vel = vel;
         this.brush = brush;
     }
-    public void upate(){
+    public void update(){
         x += vector.x * vel;
         y += -(vector.y) * vel;
         if(x < -r * 2 || x > Basic.FORM_LENGTH + r * 2){ //when bullet meet left or right side
-            vector.turnX;
+            vector.turnX();
         }if(y < -r * 2 || y > Basic.FORM_HEIGHT + r * 2){ //when bullet meet top or bottom side
-            vector.turnY;
+            vector.turnY();
         }
     } 
     /* 
@@ -213,7 +272,7 @@ public class Vector{
         this.x = (float)Math.Cos(anglePI);
         this.y = (float)Math.Sin(anglePI);
     }
-    public void turnX(){
+    public void turnY(){
         anglePI = (2.0f * (float)Math.PI) - anglePI;
         this.x = (float)Math.Cos(anglePI);
         this.y = (float)Math.Sin(anglePI);
